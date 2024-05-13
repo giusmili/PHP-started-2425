3ad79e548439a18f78f33cd80d8684f0790b231c
giusmili <giusmili67@gmail.com>
2024-05-13 16:06:08 +0200

    js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("Loaded")
-    const el_footer = document.querySelector("p")
+    const footer = document.querySelector("p")
     //console.log(el_footer.innerText)
-    sessionStorage.setItem("footer",el_footer.innerText);
-    //sessionStorage.setItem = el_footer.innerText
+    //sessionStorage.setItem("footer",el_footer.innerText);
+    sessionStorage.footer = footer.innerText
 })  
