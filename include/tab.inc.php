0d9a72312355947e8a49fa691bd55e7fc8e45c75
giusmili <giusmili67@gmail.com>
2024-05-14 14:46:02 +0200

    php

---
+++ include/tab.inc.php
@@ -0,0 +1,10 @@
+            <ul>
+				<li><strong>PHP</strong>: Hypertext Preprocessor, plus connu sous son sigle PHP, est un langage de programmation libre, principalement utilisé pour produire des pages Web dynamiques via un serveur web, mais pouvant également fonctionner comme n'importe quel langage interprété de façon locale. PHP est un langage impératif orienté objet. Wikipédia</li>
+				<li><strong>Conçu Par </strong>: Rasmus Lerdorf</li>
+				<li><strong>Dernière version</strong> : 8.3.6 (11 avril 2024)</li>
+				<li><strong>Développeurs</strong> : The PHP Group</li>
+				<li><strong>Écrit en</strong>: C</li>
+				<li><strong>Extension de fichier</strong> : php et phar</li>
+				<li><strong>Licence</strong> : Licence libre: Licence PHP</li>
+				<li><strong>Paradigmes</strong> : Impératif, orienté objet, fonctionnel, procédural, réflexif et interprété</li>
+			</ul>
